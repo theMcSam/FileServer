e3c61fcfd60faad2f26b925237e301b0e5325334
theMcSam <ardisam9876@hmail.com>
2023-05-14 19:04:32 +0000

    basic login html template

--- server/views.py
+++ server/views.py
@@ -1,16 +1,20 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
 def login(request):
-    username = request.POST["uname"]
-    password = request.POST["passwd"]
 
-    if username == 'McSam' and password == 'password':
-        return JsonResponse({'logged in successfully.'})
+    if  request.POST:
+        username = request.POST["uname"]
+        password = request.POST["passwd"]
+
+        if username == 'McSam' and password == 'password':
+            return JsonResponse({'logged in successfully.'})
+        
+    return render(request, "login.html")
     
-    return JsonResponse({'Wrong username or password.'})
+    # return JsonResponse({'Wrong username or password.'})
     
 
 def signup(request):
