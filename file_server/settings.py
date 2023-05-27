9f4045ec059051a142192074a00ffffc432be174
theMcSam <ardisam9876@hmail.com>
2023-05-27 12:11:53 +0000

    change allowed hosts and requiremnets.txt

--- file_server/settings.py
+++ file_server/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-g*f98#w2d!gwefzhvx29uzui$#v7myuo+#$*mvcot-9k4c@0iz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
