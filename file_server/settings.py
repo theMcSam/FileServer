d859d7172941718e01382e69c59ad7565af28004
theMcSam <ardisam9876@hmail.com>
2023-06-26 09:18:20 +0000

    admin satic files

--- file_server/settings.py
+++ file_server/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "assets") #place all the static files into a assets directory
+STATIC_ROOT = os.path.join(BASE_DIR, "assets/admin") #place all the static files into a assets directory
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
