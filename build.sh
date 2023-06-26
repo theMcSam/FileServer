e4058ca7ab0329ee1cd2cf9bb3319c376c885ad7
theMcSam <ardisam9876@hmail.com>
2023-06-26 09:01:40 +0000

    creating superuser on render

--- build.sh
+++ build.sh
@@ -5,5 +5,10 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
