dbd884742d690ff0dc04bc84d9dd6a7f50fabc97
theMcSam <ardisam9876@hmail.com>
2023-06-26 09:20:05 +0000

    admin satic files

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-if [ $CREATE_SUPERUSER ];
-then
-  python manage.py createsuperuser --no-input
-fi
+# if [ $CREATE_SUPERUSER ];
+# then
+#   python manage.py createsuperuser --no-input
+# fi
 
 python manage.py collectstatic --no-input
 python manage.py migrate
